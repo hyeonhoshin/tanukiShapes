96d90502aa8d58d282f9041c93fbb2cf9dd7a741
Tanukimong <35714982+Tanukimong@users.noreply.github.com>
2020-11-11 22:21:07 +0900

    Knn with data augumentation

---
+++ augument.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import imutils
+import matplotlib.pyplot as plt
+import random
+from numpy import all, any
+
+def Augument(img):
+    # Separate ROI
+    blurred = cv2.GaussianBlur(img, (5, 5), 0)
+    thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY_INV)[1]
+
+    ## Find ROI
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    cnt = cnts[0]
+
+    x,y,w,h = cv2.boundingRect(cnt)
+
+    ROI = img[x-1:x+w+1,y-1:y+h+1]
+
+    # plt.imshow(ROI,cmap='gray')
+    # plt.show()
+    
+    ## Random rotate
+    ang = np.random.randint(0,360)
+    print("Rotation(Deg) :",ang)
+    ROI_inv = cv2.threshold(ROI, 60, 255, cv2.THRESH_BINARY_INV)[1]
+    ROI_rot = imutils.rotate_bound(ROI_inv,ang)
+    ROI = cv2.threshold(ROI_rot, 60, 255, cv2.THRESH_BINARY_INV)[1]
+
+    # plt.imshow(ROI,cmap='gray')
+    # plt.show()
+
+    ## Random scaling
+    flag = True
+    scale_max = 2
+    while flag == True or any(np.array(ROI.shape) >= 300):
+        fx = random.uniform(0.5,scale_max)
+        fy = random.uniform(0.5,scale_max)
+        print("Scaling in x :",fx)
+        print("Scaling in y :",fy)
+
+        ROI = cv2.resize(ROI,dsize=(0,0), fx=fx,fy=fy)
+        flag = False
+
+        scale_max *= 0.8
+
+    # plt.imshow(ROI,cmap="gray")
+    # plt.show()
+
+    ## Random offset
+
+    patch_width = ROI.shape[0]
+    patch_height = ROI.shape[1]
+
+    left_upper_x = np.random.randint(0,300-patch_width)
+    left_upper_y = np.random.randint(0,300-patch_height)
+
+    whites = np.tile(255,(300,300))
+    whites[left_upper_x:left_upper_x+patch_width,
+                        left_upper_y:left_upper_y+patch_height] = ROI
+    img_out = whites
+
+    # plt.imshow(img_out,cmap="gray")
+    # plt.show()
+
+    return img_out
---
+++ main.py
@@ -0,0 +1,120 @@
+import cv2
+import numpy as np
+import os
+from skimage.measure import regionprops
+from skimage.transform import *
+from sklearn.model_selection import train_test_split
+from augument import Augument
+
+#create lists to save the labels (the name of the shape)
+train_labels, train_images = [],[]
+train_dir = './shapes'
+shape_list = ['circle', 'triangle', 'tetragon', 'pentagon', 'other']
+
+aug_num = 1000 # x100 dataset
+
+#function to preprocess data
+def preprocess(images, labels, augment=False):
+    """You can make your preprocessing code in this function.
+    Here, we just flatten the images, for example.
+    In addition, you can split this data into the training set and validation set for robustness to the test(unseen) data.
+
+    :params list images: (Number of images x row x column)
+    :params list labels: (Number of images, 1)
+    :rtype: array
+    :return: preprocessed images and labels
+    """
+
+    # Dimension reduction
+    #imgs = []
+    #for img in images:
+    #    imgs.append(cv2.resize(img,dsize=(128,128)))
+    #images = np.array(imgs)
+
+    dataDim = np.prod(images[0].shape)
+    images = np.array(images)
+
+    ## Data Augumentation
+    if augment == True:
+    
+        out_imgs = []
+        out_labels = []
+        
+        for i in range(images.shape[0]):
+            img = images[i]
+            for j in range(aug_num):
+                out_imgs.append(Augument(img))
+                out_labels.append(labels[i])
+
+        images = np.array(out_imgs)
+        labels = np.array(out_labels)
+
+    images = images.reshape(len(images), dataDim)
+    images = images.astype('float32')
+    #images /=255
+
+    return images, labels
+
+
+# function to make classifier
+def classify(images, labels):
+    """You can make your classifier code in this function.
+    Here, we use KNN classifier, for example.
+
+    :params array images: (Number of images x row x column)
+    :params array labels: (Number of images)
+    :return: classifier model
+    """
+    from sklearn.neighbors import KNeighborsClassifier
+    neigh = KNeighborsClassifier(n_neighbors=3)
+    neigh.fit(images, labels)
+    return neigh
+
+
+
+if __name__ == '__main__':
+    #iterate through each shape
+    for shape in shape_list:
+        print('Getting data for: ', shape)
+        for file_name in os.listdir(os.path.join(train_dir,shape)):
+            train_images.append(cv2.imread(os.path.join(train_dir,shape,file_name), 0))
+            #add an integer to the labels list
+            train_labels.append(shape_list.index(shape))
+
+    print('Number of training images: ', len(train_images))
+    
+    # Separation validation set
+    train_images, validation_images, train_labels, validation_labels = train_test_split(train_images,train_labels,test_size=0.1, random_state=999)
+
+    # Preprocess (your own function)
+    train_images, train_labels = preprocess(train_images, train_labels, augment=True)
+    validation_images, validation_labels = preprocess(validation_images, validation_labels, augment=False)
+
+    # Make a classifier (your own function)
+    model = classify(train_images, train_labels)
+
+    # Predict the labels from the model (your own code depending the output of the train function)
+    pred_labels = model.predict(validation_images)
+
+    # Calculate accuracy (Do not erase or modify here)
+    pred_acc = np.sum(pred_labels==validation_labels)/len(validation_labels)*100
+    print("Accuracy = {}".format(pred_acc))
+
+
+    """forTA (Do not erase here)
+    test_dir = '../ForTA'
+    test_labels, test_images = [], []
+    for shape in shape_list:
+        print('Getting data for: ', shape)
+        for file_name in os.listdir(os.path.join(test_dir,shape)):
+            test_images.append(cv2.imread(os.path.join(test_dir,shape,file_name), 0))
+            #add an integer to the labels list
+            test_labels.append(shape_list.index(shape))
+
+    print('Number of test images: ', len(test_images))
+
+    test_images, test_labels = preprocess(test_images, test_labels)
+    pred_labels = model.predict(test_images)
+    pred_acc = np.sum(pred_labels==test_labels)/len(test_labels)*100
+    print("Test Accuracy = {}".format(pred_acc))
+    """
