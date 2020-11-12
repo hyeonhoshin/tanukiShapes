29a756e3b8838094f31f567ef493b472ff22e2f8
Tanukimong <35714982+Tanukimong@users.noreply.github.com>
2020-11-12 16:43:31 +0900

    Add comments for submit

--- main.py
+++ main.py
@@ -11,37 +11,63 @@ shape_list = ['circle', 'triangle', 'tetragon', 'pentagon', 'other']
 
 patch_size = 32
 
-def destrained(img, cnt):
-    # img : Grayscaled img, white backgrounded is no matter
-    # Output : Straining imgs are processed
+def grab_contours(cnts):
+    # if the length the contours tuple returned by cv2.findContours
+    # is '2' then we are using either OpenCV v2.4, v4-beta, or
+    # v4-official
+    if len(cnts) == 2:
+        cnts = cnts[0]
+
+    # if the length of the contours tuple is '3' then we are using
+    # either OpenCV v3, v4-pre, or v4-alpha
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+
+    # otherwise OpenCV has changed their cv2.findContours return
+    # signature yet again and I have no idea WTH is going on
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+            "otherwise OpenCV changed their cv2.findContours return "
+            "signature yet again. Refer to OpenCV's documentation "
+            "in that case"))
+
+    # return the actual contours array
+    return cnts
+
+def de_stretch(img, cnt):
+    '''
+    Input : Grayscaled, white background img, and contours
+    Output : Aligned (Unroated) and De-streteched image
+    '''
 
     img = 255-img #Inverse image
 
+    # Align img
+    ## Extract roated angle and roate
     center, size, angle = cv2.minAreaRect(cnt)
 
     R = cv2.getRotationMatrix2D(center, angle, scale=1)
     img_rotated = cv2.warpAffine(img, R, (300, 300))
 
     cnts = cv2.findContours(img_rotated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+    cnts = grab_contours(cnts)
 
+    # De-stretched img
+    ## Find Bounding box exactly fitted
     x,y,w,h = cv2.boundingRect(cnts[0])
     img = img_rotated[y:y+h, x:x+w]
 
-    img = 255-img
-
-    # plt.imshow(img, cmap='gray')
-    # plt.show()
+    img = 255-img #Inverse image
 
     out = cv2.resize(img, dsize=(patch_size, patch_size), interpolation=cv2.INTER_LINEAR)
     out = np.array(out)
-
+    
+    # Filtering for denoising (edges have noises)
     out = cv2.bilateralFilter(out,9,100,100)
-    # plt.imshow(out, cmap='gray')
-    # plt.show()
 
-    canny = cv2.Canny(out, 100, 255)/255.0
-    out = (255-out)/255.0
+    # Feature extration
+    canny = cv2.Canny(out, 100, 255)/255.0  # Calculate boundary using Canny detecter.
+    out = (255-out)/255.0                   # Normalize img to 0 ~ 1
 
     return np.sum(out), np.sum(canny)
 
@@ -53,13 +79,13 @@ def preprocess(images, labels):
     for i, img in enumerate(images):
         thresh = cv2.threshold(img, 60, 255, cv2.THRESH_BINARY_INV)[1]
         cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
+        cnts = grab_contours(cnts)
         cnt = cnts[0]
 
         arc = cv2.arcLength(cnt, True)
         approx = cv2.approxPolyDP(cnt, 0.005 * arc, True)
 
-        area, peri = destrained(img, cnt)
+        area, peri = de_stretch(img, cnt)
 
         areas.append(area)
         peris.append(peri)
@@ -69,6 +95,14 @@ def preprocess(images, labels):
 
 # function to make classifier
 def classify(features):
+    '''
+    Rule-based classifier.
+    It classifies by the number of vertices.
+
+    Input : features (areas, peris, vertices) from preprocess stage.
+    Output : Predicted labels.
+    '''
+    # Unizip features
     areas = features[0]
     peris = features[1]
     vertices = features[2]
@@ -76,32 +110,30 @@ def classify(features):
     preds = []
     for i, num_vertices in enumerate(vertices):
         print("Classfies in...",i)
-    # if the shape is a triangle, it will have 3 vertices
-        if num_vertices == 3:
+        
+        if num_vertices == 3: # Triangle
             shape = 1
-        # if the shape has 4 vertices, it is either a square or
-        # a rectangle
-        elif num_vertices == 4:
+        
+        elif num_vertices == 4: # Rectangle
             shape = 2
-        # if the shape is a pentagon, it will have 5 vertices
-        elif num_vertices == 5:
+        elif num_vertices == 5: # Pentagon
             shape = 3
-        elif num_vertices <= 7:
+        elif num_vertices <= 7: # 6 or 7 vertices are different shape.
             shape = 4
-        else:
+        else:                   # Many vertices (includes circles and others)
 
+            # Check area is same with pi*r^2
             area = areas[i]
             r = patch_size//2
-            # Area check
             perfect_c_area = (r**2)*np.pi
             ratio = area/perfect_c_area
 
+            # Check area is same with 2*pi*r
             pred_peri = peris[i]
             perfect_peri = 2*np.pi*r
-
             ratio_peri = pred_peri/perfect_peri
 
-            if (0.94 <= ratio and ratio <= 1/0.94) and ratio_peri<1.3:
+            if (0.94 <= ratio and ratio <= 1/0.94) and ratio_peri<1.3:   # Experience-base determined thresholds.
                 shape = 0
             else:
                 shape = 4
@@ -122,13 +154,11 @@ if __name__ == '__main__':
     print('Number of training images: ', len(train_images))
 
     features, train_labels = preprocess(train_images, train_labels)
-
-    pred_labels = classify(features)
+    pred_labels = classify(features)                                    # Rule-based method do not need training step.
 
     # Calculate accuracy (Do not erase or modify here)
     pred_acc = np.sum(pred_labels==train_labels)/len(train_labels)*100
     print("Accuracy = {}".format(pred_acc))
-
 
     # TA code which is available in this code.
     """forTA (Do not erase here)
